58d1c9912bda7050eff35455d297ffc3c7ae8946
TashaSidorovaQA <natasha771@mail.ru>
2025-07-25 08:17:23 +0300

    attach

--- core/utils/attach.py
+++ core/utils/attach.py
@@ -11,7 +11,7 @@ def add_logs(browser):
     log = "".join(f'{text}\n' for text in browser.driver.get_log(log_type='browser'))
     allure.attach(log, 'browser_logs', AttachmentType.TEXT, '.log')
 
-# html-код страницы
+# html-код страниц
 def add_html(browser):
     html = browser.driver.page_source
     allure.attach(html, 'page_source', AttachmentType.HTML, '.html')
